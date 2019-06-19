0b05d233be7a68ac117168b0cc3dd11ad1e592de
Mehmet Salih Bindak <mehmetsalihbindak@gmail.com>
2019-06-19 22:46:59 +0300

    gyroscope added successfuly

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
+import { gyroscope } from "react-native-sensors";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      x: 0,
+      y: 0,
+      z: 0
+    };
+  }
+
+  componentDidMount() {
+    const subscription = gyroscope.subscribe(({ x, y, z }) => {
+      x = x.toFixed(2);
+      y = y.toFixed(2);
+      z = z.toFixed(2);
+      this.setState({ x, y, z });
+    });
+
+    this.setState({ subscription });
+  }
+
+  componentWillUnmount() {
+    this.state.subscription.unsubscribe();
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text> X ----> {this.state.x} </Text>
+        <Text> Y ----> {this.state.y} </Text>
+        <Text> Z ----> {this.state.z} </Text>
       </View>
     );
   }
@@ -17,15 +45,5 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#F5FCFF"
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
-  },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
   }
 });
